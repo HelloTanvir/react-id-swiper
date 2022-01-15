e983ba59da3ddc8e8357bb6866e1bf3d66e15e40
HelloTanvir <69837408+HelloTanvir@users.noreply.github.com>
2022-01-15 10:59:25 +0600

    react-id-swiper removed, next js built in Image component used instead of img tag, configured image source

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 
+import Image from "next/image";
 import React from "react";
 import SwiperCore, {
   EffectCube, Pagination
@@ -15,15 +16,35 @@ SwiperCore.use([EffectCube,Pagination]);
 
 export default function App() {
   return (
-    <>
-    <Swiper effect={'cube'} grabCursor={true} cubeEffect={{
-  "shadow": true,
-  "slideShadows": true,
-  "shadowOffset": 20,
-  "shadowScale": 0.94
-}} pagination={true} className="mySwiper">
-  <SwiperSlide><img src="https://swiperjs.com/demos/images/nature-1.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-2.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-3.jpg" /></SwiperSlide><SwiperSlide><img src="https://swiperjs.com/demos/images/nature-4.jpg" /></SwiperSlide>
-  </Swiper>
-    </>
+    
+    <Swiper
+        effect={'cube'}
+        grabCursor={true}
+        cubeEffect={{
+          "shadow": true,
+          "slideShadows": true,
+          "shadowOffset": 20,
+          "shadowScale": 0.94
+        }}
+        pagination={true}
+        className="mySwiper"
+    >
+        <SwiperSlide>
+            <Image src="https://swiperjs.com/demos/images/nature-1.jpg" alt="" layout="fill" />
+        </SwiperSlide>
+
+        <SwiperSlide>
+            <Image src="https://swiperjs.com/demos/images/nature-2.jpg" alt="" layout="fill" />
+        </SwiperSlide>
+
+        <SwiperSlide>
+            <Image src="https://swiperjs.com/demos/images/nature-3.jpg" alt="" layout="fill" />
+        </SwiperSlide>
+
+        <SwiperSlide>
+            <Image src="https://swiperjs.com/demos/images/nature-4.jpg" alt="" layout="fill" />
+        </SwiperSlide>
+    </Swiper>
+    
   )
 }
